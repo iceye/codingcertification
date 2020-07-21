ab53f300ac7eb282f3a898928815698fa66c7445
Michele Borzykh <michele.borzykh@docebo.com>
2020-07-21 16:50:44 +0200

    Register html and php + index link to register

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include("lib/functions.php");
 <body>
   <!-- ADD HERE YOUR HTML CODE -->    
   <h1>HOME PAGE - HELLO WORLD</h1>
-
+  <a href="register.php">register</a>
   
   <!-- JS SCRIPT INCLUSION -->
   <script
--- register.php
+++ register.php
@@ -3,6 +3,44 @@ include("lib/functions.php");
 
 /*BUSINESS LOGIC CODE*/
 
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($dblink, $_POST['username']);
+  $password_1 = mysqli_real_escape_string($dblink, $_POST['password']);
+  $password_2 = mysqli_real_escape_string($dblink, $_POST['password']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM user WHERE username='$username' LIMIT 1";
+  $result = mysqli_query($dblink, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO user (username, password) 
+  			  VALUES('$username', '$password')";
+  	mysqli_query($dblink, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
 
 
 
@@ -19,9 +57,31 @@ include("lib/functions.php");
 </head>
 
 <body>
-  <!-- ADD HERE YOUR HTML CODE -->    
-  <h1>REGISTER - HELLO WORLD</h1>
-  
+  <!-- ADD HERE YOUR HTML CODE --> 
+  <div class="header">   
+    <h1>REGISTER - HELLO WORLD</h1>
+  </div>
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a>
+  	</p>
+  </form>
   <!-- JS SCRIPT INCLUSION -->
   <script
   src="https://code.jquery.com/jquery-3.5.1.min.js"
@@ -34,3 +94,5 @@ include("lib/functions.php");
   <script src="js/register.js"></script>
 </body>
 </html>
+
+
