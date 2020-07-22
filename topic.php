1c7819b4dafc91b081ec3c463e3af12c01872378
Uzukozzi <stefano.cozzi@docebo.com>
2020-07-22 17:35:45 +0200

    Update topic.php
    Added table

--- topic.php
+++ topic.php
@@ -8,24 +8,33 @@ error_reporting(E_ALL);
 $query = "SELECT * FROM topic";
 echo "<b> <center>Database Output</center> </b> <br> <br>";
 
+echo '<center><table border="0" cellspacing="2" cellpadding="2"> 
+      <tr> 
+          <td> <font face="Arial">TopicId</font> </td> 
+          <td> <font face="Arial">title</font> </td> 
+          <td> <font face="Arial">created_at</font> </td> 
+          <td> <font face="Arial">UserId</font> </td> 
+      </tr>';
 
 if ($result = mysqli_query($dblink, $query)) {
+
     while ($row = $result->fetch_assoc()) {
         $field1name = $row["topicId"];
         $field2name = $row["title"];
         $field3name = $row["created_at"];
         $field4name = $row["userId"];
  
-        echo $field1name.'<br />';
-        echo $field2name.'<br />';
-        echo $field3name.'<br />';
-        echo $field4name.'<br />';
+        echo '<tr>
+                <td>'.$field1name.'</td>
+                <td>'.$field2name.'</td>
+                <td>'.$field3name.'</td>
+                <td>'.$field4name.'</td>
+             </tr>';
     }
- 
 /*freeresultset*/
 $result->free();
 }
-
+echo "</table></center>";
 
 /*BUSINESS LOGIC CODE END*/
 ?><!doctype html>
@@ -35,11 +44,12 @@ $result->free();
   <title>D-Board</title>
   <meta name="description" content="The D-Board project">
   <!-- CSS INCLUSION -->    
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/topic.css">
 </head>
 
 <body>
+
   <!-- ADD HERE YOUR HTML CODE -->    
   <h1>TOPIC - HELLO WORLD</h1>
   
@@ -51,7 +61,7 @@ $result->free();
   <script
   src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment-with-locales.min.js"
   crossorigin="anonymous"></script>     
-  <script src="js/scripts.js"></script>
+  <script src="js/script.js"></script>
   <script src="js/topic.js"></script>    
 </body>
 </html>
