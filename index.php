791956eaa85c693b8301e2fd10c9d761efa2752c
Adriano <v_adraqc@hotmail.com>
2020-07-25 22:33:16 +0200

    Pagination
    Created pagination using form as buttons. And CSS adjustment

--- index.php
+++ index.php
@@ -4,24 +4,18 @@ include("lib/functions.php");
 /*BUSINESS LOGIC CODE*/
 
   $title = $_POST['topicTitle'];
+  
+  $pageNumber = $_POST['pageNumberBtn'];
+  
   $userId = '1';
   $allertText = null;
   $allertType = null;
-
-  /* Query to get all topics from getTopics() function */
-  $arrayTopics = getTopics();
-  $arrayTopicsPaginated = getTopicsPaginated(10,1);
-
-  /* Getting the amount of pages to display */
-  $numberOfPages = $arrayTopicsPaginated['totalpages'];
-  ?><center>Number of Pages: <?print_r($numberOfPages);?></center></br><?
 
   /*Creation of New Topics*/
     $title = $_POST['topicTitle'];
     $userId = '1';
     /* $userID to be changed with the ID of the current logged user */
-    
-  if ($title != "") {
+    if ($title != "") {
       $checktopic = saveNewTopic($title, $userId);
         if ($checktopic == "-1") {
           $allertText="ERROR";
@@ -47,6 +41,7 @@ include("lib/functions.php");
 </head>
 
 <body>
+  
   <!-- ADD HERE YOUR HTML CODE -->    
   <p align="center">
     <a href="register.php">register</a>
@@ -55,7 +50,7 @@ include("lib/functions.php");
     <h1>Discussions</h1>
   </div>
   
-  <form method="post" action="index.php" id="topic_creation">
+  <form method="post" action="index.php" id="topic_creation" class="content">
 
     <?php include('errors.php'); ?>
     
@@ -63,20 +58,28 @@ include("lib/functions.php");
       <formTitle>Create a discussion</formTitle>
       <label>Title</label>
         <input type="text" name="topicTitle" id="topicTitle" required>
-      </div>
-        <button type="submit" class="btn" name="new_topic">Add Discussion</button>
-    </form>
+        <br><button type="submit" class="btn" name="new_topic">Add Discussion</button>
+        </div>
+      </form>
     </div>
 
 <?php
   /* Table with paginated topics */
       echo '<table>';
-      $arrayTopicsPaginated = getTopicsPaginated(10,1);
+
+  /*    $arrayTopicsPaginated = getTopicsPaginated(10,$pageNumber);*/
 
       /* Getting the amount of pages to display */
-      $numberOfPages = $arrayTopicsPaginated['totalpages'];
-      ?><center>Number of Pages: <?print_r($numberOfPages);?></center></br><?
 
+      if ($pageNumber == 0 or Null){
+        $pageNumber = 1;
+      }
+
+      $arrayTopicsPaginated = getTopicsPaginated(10,$pageNumber);      
+      $numberOfPages = $arrayTopicsPaginated['totalpages'];
+      /* echo to check total number of pages is retrieved correctly */
+      /* echo('<h2>num pages prima: '.$numberOfPages.'</h2>'); */
+      
         foreach ($arrayTopicsPaginated['data'] as $item) {
           $idFromArray = $item['userId'];
           $arrayUserFromId = getUserById($idFromArray);
@@ -89,31 +92,23 @@ include("lib/functions.php");
 ?>
 
 <?php
-/* Create links to pages - draft*/
-?>
-<div class="page-links">Page:
-  <?
-  for ($i = 1; $i <= $numberOfPages; $i++) {
-      echo $i.', ';
-    }
-?>
-</div>
+  /* Create links to pages - draft*/
+  ?>
+  <form method="post" id="pager" class="numeriDiPagina">
+    <?
+    for ($i = 1; $i <= $numberOfPages; $i++) {
 
-<?php
-/* Table with ALL topics */
-  echo '<table>';
+      if ($i == $pageNumber) {
+        echo '<button class="pageBtn" disabled>'.$i.'</button>';
+      }
+      else{
+        echo '<button type="submit" class="pageBtnNo" name="pageNumberBtn" value="'.$i.'">'.$i.'</button>';
 
-    foreach ($arrayTopics['data'] as $item) {
-      $idFromArray = $item['userId'];
-      $arrayUserFromId = getUserById($idFromArray);
+      }
+  }
 
-      echo '<tr>
-      <td>Title: '.$item['title'].'</br>Created by: '.$arrayUserFromId['username'].'Created at: '.$item['created_at'].'</td>
-      </tr>';
-    }
-  
-    echo '</table>';
 ?>
+</form>
 
 <!-- JS SCRIPT INCLUSION -->
 <script
