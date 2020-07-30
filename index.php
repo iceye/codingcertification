fb58b49fc826e689b63f82cfede702dbf266480f
Adriano <v_adraqc@hotmail.com>
2020-07-30 13:02:25 +0200

    Update index.php
    Fixed url generation to open topics.

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ $allertType = null;
         $arrayUserFromId = getUserById($idFromArray);
 
        echo '<tr>
-       <td><a href=/topic.php?topicID='.$item['topicId'].'>'.$item['title'].'</a></br>Created by: '.$arrayUserFromId['username'].'Created at: '.$item['created_at'].'</td>
+       <td><a href=./topic.php?topicID='.$item['topicId'].'>'.$item['title'].'</a></br>Created by: '.$arrayUserFromId['username'].'Created at: '.$item['created_at'].'</td>
        </tr>';    
   }
 ?>
